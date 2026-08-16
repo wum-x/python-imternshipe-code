a4ec989e32f615551adb774cbff971cfd5753653
wum-x <sableshivraj0@gmail.com>
2026-08-16 14:50:26 +0530

    basic C program that prints 'Hello, World!'

---
+++ day-03_27-05-2026/baciprograme.c
@@ -0,0 +1,12 @@
+// Basic C Program
+
+#include <stdio.h>  // Header file for input and output
+
+int main() {
+
+    // Print a message on the screen
+    printf("Hello, World!");
+
+    // Return 0 means the program ended successfully
+    return 0;
+}
