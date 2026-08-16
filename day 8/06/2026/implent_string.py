d2f68d4754260dadc0a54abc932976534b5386e6
wum-x <sableshivraj0@gmail.com>
2026-08-16 14:43:24 +0530

    Implement basic string operations in Python

---
+++ day 8/06/2026/implent_string.py
@@ -0,0 +1,33 @@
+# Create a string
+name = "Python"
+
+# Print the string
+print("String:", name)
+
+# Find the length of the string
+print("Length:", len(name))
+
+# Access the first character
+print("First character:", name[0])
+
+# Access the last character
+print("Last character:", name[-1])
+
+# Convert string to uppercase
+print("Uppercase:", name.upper())
+
+# Convert string to lowercase
+print("Lowercase:", name.lower())
+
+# Replace a character
+print("Replace:", name.replace("P", "J"))
+
+# Check whether a word exists
+if "Py" in name:
+    print("Py is present")
+
+# String slicing
+print("Slicing:", name[0:3])
+
+# Reverse the string
+print("Reverse:", name[::-1])
