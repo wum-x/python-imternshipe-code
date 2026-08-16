966112f1ef4d77d6170985dc526c0038fdfc7781
wum-x <sableshivraj0@gmail.com>
2026-08-16 14:19:08 +0530

    if-else statement for number sign checking
    Implement if-else statement to check number sign.

---
+++ day 1/06/2026/if_else_statment.py
@@ -0,0 +1,16 @@
+# IF-ELSE STATEMENT
+
+# Take a number from the user
+num = int(input("Enter a number: "))
+
+# Check if the number is positive
+if num > 0:
+    print("Positive number")
+
+# Check if the number is negative
+elif num < 0:
+    print("Negative number")
+
+# If neither condition is true
+else:
+    print("Zero")
